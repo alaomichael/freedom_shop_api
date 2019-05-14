3e2cc216a703fcbbde5501b77f5d9530a7c7e304
alaomichael <contactleomax@gmail.com>
2019-05-14 11:12:02 +0100

    Update with the addition of database file

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ var express = require('express');
 var app = express();
 var bodyParser = require ('body-parser');
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/freedom-shop')
+var db = mongoose.connect('mongodb://localhost/freedom-shop');
 
 var Product = require('./model/product');
 var WishList = require ('./model/wishlist');
+
+//Allow all requests from all domains & localhost
+app.all('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET");
+    next();
+});
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -14,11 +23,11 @@ app.post('/product', function(request,response){
     var product = new Product();
     product.title = request.body.title;
     product.price  = request.body.price;
-    product.save(function(err,saveProduct){
+    product.save(function(err,savedProduct){
         if (err){
             response.status(500).send({error:"Could not save product"});
         } else {
-            response.send(saveProduct);
+            response.send(savedProduct);
         }
     });
 });
@@ -35,12 +44,11 @@ app.get('/product', function(request,response){
 });
 
 app.get('/wishlist', function(request,response) {
- WishList.find({}).populate({path:'products', model: 'Product'}).exec(function(err, wishLists)
- {
+ WishList.find({}).populate({path:'products', model: 'Product'}).exec(function(err, wishLists) {
      if (err) {
          response.status(500).send({ error: "Could not fetch the wishlist" });
      } else {
-         response.send(wishLists);
+         response.status(200).send(wishLists);
      }
  });
 });
@@ -49,36 +57,32 @@ app.post('/wishlist', function(request, response){
    var wishList = new WishList();
    wishList.title = request.body.title;
 
-   wishList.save(function(err,newWishList){
-       if (err){
+   wishList.save(function(err,newWishList) {
+       if (err) {
            response.status(500).send({error:"Could not create wish list"});
-       } else{
+       } else {
            response.send(newWishList);
-
        }
    });
 });
 
 
-app.put('/wishlist/product/add', function(request,response){
+app.put('/wishlist/product/add', function(request,response) {
 Product.findOne({_id: request.body.productId}, function(err,product){
     if(err){
         response.status(500).send({error:"Could not add item to wishlist"});
     } else {
-        WishList.update({_id:request.body.wishListId}, {$addToSet:
-        {products: product._id}},
-        function(err,wishList){
+        WishList.update({_id:request.body.wishListId}, {$addToSet:{products: product._id}}, function(err,wishList) {
             if(err) {
                 response.status(500).send({error:"Could not update wishlist"});
             } else{
-                response.send(wishList);
+                response.send("Successfully added to wishlist");
             }
         });
     }
-});
+})
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
     console.log("Freedom Shop API running on port 3000...");
-
 });
